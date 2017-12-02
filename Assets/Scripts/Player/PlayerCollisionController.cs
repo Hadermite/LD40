dd75f44d578f2e8cbe9ce2b21abb251db0166732
Lukas Wiklund <lukas@wiklund.se>
2017-12-02 04:24:09 +0100

    First day

---
+++ Assets/Scripts/Player/PlayerCollisionController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class PlayerCollisionController : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.gameObject.name == "Player House") {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovementController : MonoBehaviour {
+
+    private const float MaxSpeed = 10f;
+    private const float RunMultiplier = 2f;
+    
+    private Rigidbody2D body;
+
+    private void Start() {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        Vector3 movement = new Vector3 {
+            x = Input.GetAxisRaw("Horizontal"),
+            y = Input.GetAxisRaw("Vertical")
+        };
+
+        if (movement.magnitude != 0) {
+            float multiplier = 1;
+            if (Input.GetKey(KeyCode.LeftShift)) {
+                multiplier = RunMultiplier;
+            }
+            movement = movement.normalized * MaxSpeed * Time.deltaTime * multiplier;
+            body.MovePosition(transform.position + movement);
+        }
+    }
+}
