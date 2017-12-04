b618cc6eef45046391e6e2bdde33a4f4d1848758
Lukas Wiklund <lukas@wiklund.se>
2017-12-04 17:05:15 +0100

    Bug fixes

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,6 @@
 ﻿public class GameController {
 
-    public const int MoneyNeededToWin = 0;
+    public const int MoneyNeededToWin = 10000;
 
     public static float totalTimer = 0;
     public static int totalEarnedMoney = 0;
